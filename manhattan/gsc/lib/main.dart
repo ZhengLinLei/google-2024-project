46be0d59f93b6cd99ec6b4847b114f2251218099
achechi15 <142111179+achechi15@users.noreply.github.com>
2023-12-22 13:18:02 +0100

    Config files

--- manhattan/gsc/lib/main.dart
+++ manhattan/gsc/lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(
+        title: const Text('Inicio Sesion Denuncia Ciudadana'),
       ),
+      body: const SignUpPage(),
+      )
+      
+
     );
   }
 }
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SizedBox(
+            height: 300,
+            width: 300,
+            child: Image.asset('assets/chipiChapa.jpg'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Register'),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Sign Up'),
+          )
+        ]
+      )
+    );
+  }
+}
+
+
